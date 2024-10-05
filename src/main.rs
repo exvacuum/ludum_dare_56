a1baa49a49cb3c727ec7d26fe7d7872d6c579a56
Silas Bartha <silas@exvacuum.dev>
2024-10-05 11:31:43 -0400

    Added Embedded Asset Module

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,15 @@ use ui::*;
 mod player;
 use player::*;
 
+mod bugoid;
+use bugoid::*;
+
+mod world;
+use world::*;
+
+mod embedded_assets;
+use embedded_assets::*;
+
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum AppState {
     #[default]
@@ -26,58 +35,26 @@ pub enum PausedState {
 pub struct GameplaySet;
 
 #[derive(Component, Debug)]
-struct Spin(pub f32);
-
-#[derive(Component, Debug)]
 struct GameObject;
 
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, UiPlugin))
+    let mut app = App::new();
+        app.add_plugins((DefaultPlugins, UiPlugin, WorldPlugin, PlayerPlugin))
         .init_state::<AppState>()
         .init_state::<PausedState>()
-        .add_systems(Startup, hello_world)
-        .add_systems(OnEnter(AppState::InGame), setup_basic_scene)
         .add_systems(OnExit(AppState::InGame), clean_up_game)
-        .add_systems(Update, (spin).in_set(GameplaySet))
         .configure_sets(
             Update,
             (GameplaySet
                 .run_if(in_state(AppState::InGame))
                 .run_if(in_state(PausedState::Running)),),
-        )
-        .run();
-}
-
-fn setup_basic_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        GameObject,
-        Camera2dBundle::default(),
-    ));
-    commands.spawn((
-        GameObject,
-        MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(asset_server.add(Rectangle::new(100.0, 100.0).mesh().build())),
-            material: asset_server.add(ColorMaterial::from_color(Color::srgb(1.0, 0.0, 0.0))),
-            ..Default::default()
-        },
-        Spin(PI),
-    ));
+        );
+        embed_assets(&mut app);
+        app.run();
 }
 
 fn clean_up_game(mut commands: Commands, query: Query<Entity, With<GameObject>>) {
     for entity in query.iter() {
         commands.entity(entity).despawn_recursive();
-    }
-}
-
-fn hello_world() {
-    info!("Hello World!");
-}
-
-fn spin(mut spinner_query: Query<(&mut Transform, &Spin)>, time: Res<Time>) {
-    let delta = time.delta_seconds();
-    for (mut spinner_transform, spin) in spinner_query.iter_mut() {
-        spinner_transform.rotate_z(spin.0 * delta);
     }
 }
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,9 +1,22 @@
 use bevy::prelude::*;
 use leafwing_input_manager::plugin::InputManagerPlugin;
 
+use crate::{AppState, GameObject};
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(AppState::InGame), setup_player);
     }
+}
+
+fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        GameObject,
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 5.0, 10.0)).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+    ));
 }
